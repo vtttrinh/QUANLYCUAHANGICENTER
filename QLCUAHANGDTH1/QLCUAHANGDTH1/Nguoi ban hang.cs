ccd97c14f7439218e4ad19e58c20d665ac094e9f
tranhoaiviet <hangul309@gmail.com>
2021-11-08 21:41:55 +0700

    QLCUAHANGDTH đã sửa

---
+++ QLCUAHANGDTH1/QLCUAHANGDTH1/Nguoi ban hang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCUAHANGDTH1
+{
+    public partial class Nguoi_ban_hang : Form
+    {
+        public Nguoi_ban_hang()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QLCUAHANGDTH1/QLCUAHANGDTH1/quan ly tai khoan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCUAHANGDTH1
+{
+    public partial class quan_ly_tai_khoan : Form
+    {
+        public quan_ly_tai_khoan()
+        {
+            InitializeComponent();
+        }
+    }
+}
